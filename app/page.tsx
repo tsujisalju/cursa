c30c8213ce86a77da9789818afcf6b1273cfbbe3
purrnama <purrnama@proton.me>
2025-12-13 03:15:50 +0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,50 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    return (
+        <div className="flex flex-col min-h-screen justify-center bg-zinc-50 dark:bg-zinc-900">
+            <div className="flex flex-row w-full h-max p-16">
+                <div className="flex flex-col grow space-y-4">
+                    <Image
+                        src="textlogo-metallic.svg"
+                        alt="tsujisalju logo"
+                        width={500}
+                        height={100}
+                    />
+                    <div className="flex flex-col">
+                        <h1 className="font-sans font-light text-5xl">
+                            Have faith in the creator, and our ability to
+                            create.
+                        </h1>
+                        <p className="font-serif text-lg mt-2">
+                            I’m Qayyum, a freelance developer and designer based
+                            in Johore, Malaysia.
+                        </p>
+                        <p className="font-serif text-lg">
+                            Telling my story through mythical creatures from a
+                            world not so different than ours.
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-col justify-between text-end">
+                    <Link href="#" className="font-sans font-bold text-2xl">
+                        Works
+                    </Link>
+                    <Link href="#" className="font-sans font-bold text-2xl">
+                        Personal
+                    </Link>
+                    <Link href="#" className="font-sans font-bold text-2xl">
+                        Almanac
+                    </Link>
+                    <Link href="#" className="font-sans font-bold text-2xl">
+                        Logs
+                    </Link>
+                </div>
+            </div>
+            <div className="flex flex-col grow px-16">
+                <h1 className="font-bold text-3xl">Works</h1>
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
